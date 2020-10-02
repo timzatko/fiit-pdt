5499e6393320bb203f4bd1cec798a782add3dd56
Timotej Zaťko <timi.zatko@gmail.com>
2020-10-02 10:06:14 +0200

    Add unique constraint to tweet_hashtags

--- db/migrations/20200928115833_create_tweet_hashtags_table.up.sql
+++ db/migrations/20200928115833_create_tweet_hashtags_table.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE tweet_hashtags (
     id					SERIAL PRIMARY KEY,
     hashtag_id			integer,
-    tweet_id			varchar(20)
+    tweet_id			varchar(20),
+    UNIQUE (hashtag_id, tweet_id)
 );
 
 ALTER TABLE tweet_hashtags
