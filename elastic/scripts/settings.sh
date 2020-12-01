048d0f2db6b4eb4fee49f6527deada23293027fc
Timotej Zatko <timi.zatko@gmail.com>
2020-12-01 22:15:46 +0100

    Fix closing of the index

--- elastic/scripts/settings.sh
+++ elastic/scripts/settings.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+curl --request POST http://localhost:9200/tweets/_close
+
 curl --header "Content-Type: application/json" \
   --request PUT \
   --data "$(cat "$(dirname "$0")/tweets/settings.json")" \
   http://localhost:9200/tweets/_settings
+
+curl --request POST http://localhost:9200/tweets/_open
